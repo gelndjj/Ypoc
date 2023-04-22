73c7dd10b4fef71101542583eea666a479d27823
gelndjj <silence_ps@hotmail.fr>
2023-04-22 21:05:12 +0200

    add percent copy progress labels /add buttons quit, projects, kofi

--- ypoc_gui.py
+++ ypoc_gui.py
@@ -1,7 +1,5 @@
-import os
-import shutil
-import tkinter as tk
-from tkinter import filedialog, ttk
+import tkinter as tk, shutil, webbrowser, os
+from tkinter import filedialog
 from tkinter import *
 import customtkinter as ck
 from PIL import ImageTk, Image
@@ -18,36 +16,117 @@ window.geometry("%dx%d+%d+%d" % (width, height, x, y))
 window.resizable(0, 0)
 
 
-def cp_tree():
+def cp_tree():# DONE
     src_folder = filedialog.askdirectory(title="Select Source Folder")
 
     if src_folder:
         dst_folder = filedialog.askdirectory(title="Select Destination Folder")
 
         if dst_folder:
+            # Count the total number of directories that need to be created
+            total_dirs = sum([len(dirs) for _, dirs, _ in os.walk(src_folder)])
+            created_dirs = 0
+
+            # create a flag to stop the copy process
+            stop_flag = False
+            def stop_copy():
+                nonlocal stop_flag
+                stop_flag = True
+
+            # bind the 'A' key to the stop_copy() function
+            window.bind('a', lambda event: stop_copy())
+
             for dirpath, dirnames, filenames in os.walk(src_folder):
+                if stop_flag:
+                    break
                 relative_path = os.path.relpath(dirpath, src_folder)
                 new_dir_path = os.path.join(dst_folder, relative_path)
                 os.makedirs(new_dir_path, exist_ok=True)
+                created_dirs += 1
 
-            tk.messagebox.showinfo("Success", "Folder structure copied successfully!")
+                # calculate and update percentage
+                percent = int((created_dirs / total_dirs) * 100)
+                percent_str = f"{percent}%"
+                percent_label.configure(text=percent_str)
+                percent_label.update()
+
+                abort_str = 'Press \'a\' to abort'
+                abort_label.configure(text=abort_str)
+
+            percent_label.configure(text="")
+            abort_label.configure(text="")
+
+            if stop_flag:
+                tk.messagebox.showinfo("Stop", "Copy process was stopped.")
+            else:
+                tk.messagebox.showinfo("Success", "Folder structure copied successfully!")
 
 def cp_files():
     source = filedialog.askdirectory(initialdir="/", title="Select Source")
     destination = filedialog.askdirectory(initialdir="/", title="Select Destination")
 
-    shutil.copytree(source, destination, dirs_exist_ok=True)
+    total_size = sum(os.path.getsize(os.path.join(foldername, filename)) for foldername, subfolders, filenames in os.walk(source) for filename in filenames)
+    copied_size = 0
+    stop_flag = False  # create a flag to stop the copy process
+
+    def stop_copy():
+        nonlocal stop_flag
+        stop_flag = True
+
+    # bind the 'A' key to the stop_copy() function
+    window.bind('a', lambda event: stop_copy())
+
+    for foldername, subfolders, filenames in os.walk(source):
+        for filename in filenames:
+            src_file = os.path.join(foldername, filename)
+            dest_file = src_file.replace(source, destination, 1)
+
+            # Create the destination directory if it does not exist
+            os.makedirs(os.path.dirname(dest_file), exist_ok=True)
+
+            # Check if the stop flag is set and stop the copy process if it is
+            if stop_flag:
+                tk.messagebox.showinfo("Stop", "Copy process was stopped.")
+                percent_label.configure(text="")
+                abort_label.configure(text="")
+                return
+
+            # Copy the file and update the progress label
+            shutil.copy2(src_file, dest_file)
+            copied_size += os.path.getsize(src_file)
+            percent = int(copied_size/total_size * 100)
+            percent_label.configure(text=f"{percent}%")
+            percent_label.update()
+
+            abort_str = 'Press \'a\' to abort'
+            abort_label.configure(text=abort_str)
+
+    percent_label.configure(text="")
+    abort_label.configure(text="")
 
     tk.messagebox.showinfo("Copy Complete", "The files have been copied successfully.")
 
-def cp_newer():
+def cp_newer(): #DONE
     source_dir = filedialog.askdirectory(title="Select source directory")
     destination_dir = filedialog.askdirectory(title="Select destination directory")
+
+    total_size = sum(os.path.getsize(os.path.join(root_dir, file)) for root_dir, _, files in os.walk(source_dir) for file in files)
+    bytes_copied = 0
+
+    stop_flag = False
+    def stop_copy():
+        nonlocal stop_flag
+        stop_flag = True
+
+    # bind the 'A' key to the stop_copy() function
+    window.bind('a', lambda event: stop_copy())
 
     for root_dir, _, files in os.walk(source_dir):
         dest_dir = os.path.join(destination_dir, os.path.relpath(root_dir, source_dir))
         os.makedirs(dest_dir, exist_ok=True)
         for file in files:
+            if stop_flag:
+                break
             src_file = os.path.join(root_dir, file)
             dest_file = os.path.join(dest_dir, file)
             # Copy the file if it is newer
@@ -56,42 +135,131 @@ def cp_newer():
                     shutil.copy2(src_file, dest_file)
             else:
                 shutil.copy2(src_file, dest_file)
+            file_size = os.path.getsize(src_file)
+            bytes_copied += file_size
+            percent = int(bytes_copied / total_size * 100)
+            percent_label.configure(text=f"{percent}%")
+            percent_label.update()
 
-    tk.messagebox.showinfo("Success", "Newer items copied successfully!")
+            abort_str = 'Press \'a\' to abort'
+            abort_label.configure(text=abort_str)
 
-def gather():
+            # check stop_flag and break out of loop if True
+            if stop_flag:
+                break
+
+        percent_label.configure(text="")
+        abort_label.configure(text="")
+
+    if stop_flag:
+        tk.messagebox.showinfo("Copy Aborted", "The copy process was aborted.")
+    else:
+        tk.messagebox.showinfo("Success", "Newer items copied successfully!")
+
+def gather(): #DONE
     folder_path = filedialog.askdirectory()
     file_paths = []
 
+    total_files = 0
+    copied_files = 0
+
+    # Count the total number of files to copy
+    for root, dirs, files in os.walk(folder_path):
+        total_files += len(files)
+
+    # create a flag to stop the copy process
+    stop_flag = False
+    def stop_copy():
+        nonlocal stop_flag
+        stop_flag = True
+
+    # bind the 'A' key to the stop_copy() function
+    window.bind('a', lambda event: stop_copy())
+
     for root, dirs, files in os.walk(folder_path):
         for file in files:
+            if stop_flag: # check if stop flag is set
+                break # stop copying files
             file_paths.append(os.path.join(root, file))
 
-    with open('list_files.txt', 'w') as f:
-            for item in file_paths:
-                f.write('%s\n' % item)
-    
-    tk.messagebox.showinfo("Success", "Paths list copied successfully!")
+            copied_files += 1
+            progress_text = "{}/{}".format(copied_files, total_files)
+            # Update the label with the progress text
+            percent_label.configure(text=progress_text)
+            # Update the label immediately
+            percent_label.update()
+            abort_str = 'Press \'a\' to abort'
+            abort_label.configure(text=abort_str)
 
-def edited_files():
+        if stop_flag: # check if stop flag is set
+            break # stop copying files
+
+    with open('list_files.txt', 'w') as f:
+        for item in file_paths:
+            if stop_flag: # check if stop flag is set
+                break # stop writing file paths
+            f.write('%s\n' % item)
+
+            copied_files += 1
+            progress_text = "{}/{}".format(copied_files, total_files)
+            # Update the label with the progress text
+            percent_label.configure(text=progress_text)
+            # Update the label immediately
+            percent_label.update()
+
+            abort_str = 'Press \'a\' to abort'
+            abort_label.configure(text=abort_str)
+
+        percent_label.configure(text="")
+        abort_label.configure(text="")
+
+    if stop_flag:
+        tk.messagebox.showwarning("Aborted", "Copy process aborted.")
+    else:
+        tk.messagebox.showinfo("Success", "Paths list copied successfully!")
+
+def edited_files(): #DONE
     source_dir = filedialog.askdirectory(title="Select source directory")
     destination_dir = filedialog.askdirectory(title="Select destination directory")
+
+    total_files = sum([len(files) for root_dir, _, files in os.walk(source_dir)])
+    copied_files = 0
+    stop_flag = False
+
+    def stop_copy(event=None):
+        nonlocal stop_flag
+        stop_flag = True
+    window.bind("a", stop_copy)
 
     for root_dir, _, files in os.walk(source_dir):
         dest_dir = os.path.join(destination_dir, os.path.relpath(root_dir, source_dir))
         if not os.path.exists(dest_dir):
             continue
         for file in files:
+            if stop_flag:
+                tk.messagebox.showinfo("Stop", "Copy process aborted.")
+                abort_label.configure(text="")
+                percent_label.configure(text="")
+                return
             src_file = os.path.join(root_dir, file)
             dest_file = os.path.join(dest_dir, file)
             if not os.path.exists(dest_file):
                 continue
             if os.path.getmtime(src_file) > os.path.getmtime(dest_file):
                 shutil.copy2(src_file, dest_file)
+            copied_files += 1
+            progress_percent = int(copied_files / total_files * 100)
+            percent_label.configure(text=f"{progress_percent}%")
+            percent_label.update()
+            abort_str = 'Press \'a\' to abort'
+            abort_label.configure(text=abort_str)
+
+    percent_label.configure(text="")
+    abort_label.configure(text="")
 
     tk.messagebox.showinfo("Success", "Edited files copied successfully!")
 
-def delete_files():
+def delete_files(): #DONE
     folder_path = filedialog.askdirectory(title="Select source directory")
     count = 0
     for filename in os.listdir(folder_path):
@@ -107,17 +275,54 @@ def delete_files():
     
     tk.messagebox.showinfo("Success", f"Files deleted successfully!\n Files left: {count}")
 
-def move_files():
+
+def move_files(): #DONE
     source_dir = filedialog.askdirectory(title="Select source directory")
     destination_dir = filedialog.askdirectory(title="Select destination directory")
+
+    stop_flag = False
+
+    def stop_moving(event):
+        nonlocal stop_flag
+        stop_flag = True
+
+    window.bind('a', stop_moving)
+
+    num_files = len(os.listdir(source_dir))
+    completed = 0
+
     for file_name in os.listdir(source_dir):
+        if stop_flag:
+            break
         file_path = os.path.join(source_dir, file_name)
         if os.path.isfile(file_path):
-            os.rename(file_path, os.path.join(destination_dir, file_name))
+            shutil.move(file_path, os.path.join(destination_dir, file_name))
         elif os.path.isdir(file_path):
-            os.rename(file_path, os.path.join(destination_dir, file_name))
+            shutil.move(file_path, os.path.join(destination_dir, file_name))
 
-    tk.messagebox.showinfo("Success", "Files moved successfully!")
+        completed += 1
+        progress_percent = (completed / num_files) * 100
+        percent_label.configure(text=f"{progress_percent:.1f}%")
+        percent_label.update()
+
+        abort_str = 'Press \'a\' to abort'
+        abort_label.configure(text=abort_str)
+
+    percent_label.configure(text="")
+    abort_label.configure(text="")
+
+    if not stop_flag:
+        tk.messagebox.showinfo("Success", "Files moved successfully!")
+    else:
+        tk.messagebox.showinfo("Abort", "Moving files aborted!")
+
+    window.unbind('a')
+
+def open_github_profile():
+    webbrowser.open_new("https://github.com/gelndjj")
+
+def kofi():
+    webbrowser.open_new("https://ko-fi.com/gelndjj")
 
 frame = tk.Frame(window, width=800, height=500, borderwidth=0, highlightthickness=0)
 frame.pack()
@@ -131,6 +336,12 @@ canvas.create_image(0, 0, anchor=tk.NW, image=img)
 canvas2 = tk.Canvas(frame, width=700, height=400, highlightthickness=0)
 canvas2.place(x=200,y=100)
 canvas2.create_rectangle(0, 0, 698, 398, outline="black", width=6)
+
+percent_label = ck.CTkLabel(canvas2, text='',bg_color='#ebeceb', text_color='red')
+percent_label.place(x=15,y=205)
+
+abort_label = ck.CTkLabel(canvas2, text='',bg_color='#ebeceb', text_color='red')
+abort_label.place(x=130,y=205)
 
 cp_tree_img = ck.CTkImage(light_image=Image.open("img_soft/cp_tree.png"),
                                   size=(45, 45))
@@ -161,5 +372,11 @@ btn_del = ck.CTkButton(canvas2, text='Wipe Out\n Destination', width=100, height
 btn_del.place(x=120, y=230)
 btn_mv = ck.CTkButton(canvas2, text='Move\n Files', width=132, height=75, fg_color='white', hover_color='#9adffc', text_color='black',image=cp_mv_img, border_width=2, border_color='#d7dee0', command=move_files)
 btn_mv.place(x=120, y=306)
+btn_git = ck.CTkButton(frame, text='Other Projects', width=60, fg_color='#101a24', bg_color='#0f1b24', hover_color='blue', text_color='white', border_width=2, border_color='black', command=open_github_profile)
+btn_git.place(x=10, y=435)
+btn_kofi = ck.CTkButton(frame, text='Tip ☕️', width=40, fg_color='#101a24', bg_color='#0f1b24', hover_color='blue', text_color='white', border_width=2, border_color='black', command=kofi)
+btn_kofi.place(x=10, y=400)
+btn_quit = ck.CTkButton(frame, text='Quit', width=130, fg_color='#101a24', bg_color='#0f1b24', hover_color='red', text_color='white', border_width=2, border_color='black', command=lambda:window.destroy())
+btn_quit.place(x=10, y=470)
 
 window.mainloop()
